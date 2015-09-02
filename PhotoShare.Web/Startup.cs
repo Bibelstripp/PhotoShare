538e7232edf3c03eeb1322a9e0973cfa13a1a774
iRols <ragnhildo1@gmail.com>
2015-09-02 21:53:25 +0200

    PhotoShare

---
+++ PhotoShare.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhotoShare.Web.Startup))]
+namespace PhotoShare.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
